458e3369e58f7dd8e8b7267aab1114c2bed06bca
ArturVoldaev <79632566+ArturVoldaev@users.noreply.github.com>
2023-07-07 14:39:40 -0400

    Add files via upload

--- Algoritms/HomeWork/Lesson3/HomeWork3.java
+++ Algoritms/HomeWork/Lesson3/HomeWork3.java
@@ -1,21 +1,40 @@
 package homework3;
 
+import java.util.Arrays;
+
 public class HomeWork3 {
   public static void main(String[] args) {
-    int fib_len = 9;
-    System.out.print("Fibonacci row of " + fib_len + " numbers is: \n");
-    for (int i = 0; i < fib_len; i++) {
-      System.out.print(fibonacciRecursion(i) + " ");
-    }
+    fibonacciRecursion(10);
   }
-
-  public static int fibonacciRecursion(int n) {
-    if (n == 0) {
-      return 0;
+  private static int[] fibonacciRecursion(int n) {
+    if (n == 2) { // base case
+      return new int[]{0, 1};
     }
     if (n == 1) {
-      return 1;
+      return new int[]{0};
     }
-    return fibonacciRecursion(n - 1) + fibonacciRecursion(n - 2);
+    int[] result = fibonacciRecursion(n - 1);      // recursive case
+    System.out.println(Arrays.toString(result));
+    result = Arrays.copyOf(result, n);             // increase the array size
+    result[n - 1] = result[n - 2] + result[n - 3]; // sum of 2 previous elements
+    return result;
   }
+
+//  public static void main(String[] args) {
+//    int fib_len = 9;
+//    System.out.print("Fibonacci row of " + fib_len + " numbers is: \n");
+//    for (int i = 0; i < fib_len; i++) {
+//      System.out.print(fibonacciRecursion(i) + " ");
+//    }
+//  }
+//
+//  public static int fibonacciRecursion(int n) {
+//    if (n == 0) {
+//      return 0;
+//    }
+//    if (n == 1) {
+//      return 1;
+//    }
+//    return fibonacciRecursion(n - 1) + fibonacciRecursion(n - 2);
+//  }
 }
