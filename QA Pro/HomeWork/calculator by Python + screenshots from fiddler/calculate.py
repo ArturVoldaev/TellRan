2dafbff33d61b25e9e539f93fcbee86294082c08
ArturVoldaev <79632566+ArturVoldaev@users.noreply.github.com>
2023-08-02 15:18:57 -0400

    unit test by python

--- QA Pro/HomeWork/calculator by Python + screenshots from fiddler/calculate.py
+++ QA Pro/HomeWork/calculator by Python + screenshots from fiddler/calculate.py
@@ -1,29 +1,67 @@
-print("Input number A")
-numberA = int(input())
-print("Input number B")
-numberB = int(input())
-print("Choose math action:")
-print("+")
-print("-")
-print("*")
-print("/")
-userAction = input()
+import unittest
+import random
 
-def calculate(userAction, numberA, numberB):
-    if userAction == "+":
-        print(numberA + numberB)
-    if userAction == "-":
-        print(numberA - numberB)
-    if userAction == "*":
-        print(numberA * numberB)
-    if userAction == "/":
-        if numberB == 0:
-            while numberB <= 0:
-                print("Input number B. NumberB MUST be > 0")
-                print("Input number B")
+# print("Input number A")
+# numberA = int(input())
+# print("Input number B")
+# numberB = int(input())
+# print("Choose math action:")
+# print("+")
+# print("-")
+# print("*")
+# print("/")
+# userAction = input()
+
+class Calculator(unittest.TestCase):
+    def test_caculate_all_actions(self):
+        self.assertEqual(self.calculate("+", 2, 2), 4)
+        self.assertEqual(self.calculate("-", 2, 2), 0)
+        self.assertEqual(self.calculate("*", 2, 2), 4)
+        self.assertEqual(self.calculate("/", 2, 2), 1)
+
+    def test_caculate_user_action_not_sign(self):
+        self.assertEqual(self.calculate("b", 2, 2), "default case")
+
+    def test_random_sum(self):
+        first_random_int = random.randint(0, 100)
+        second_random_int = random.randint(0, 100)
+        result = first_random_int + second_random_int
+        self.assertEqual(self.calculate("+", first_random_int, second_random_int), result)
+
+    def test_random_min(self):
+        first_random_int = random.randint(0, 100)
+        second_random_int = random.randint(0, 100)
+        result = first_random_int - second_random_int
+        self.assertEqual(self.calculate("-", first_random_int, second_random_int), result)
+
+    def test_random_mul(self):
+        first_random_int = random.randint(0, 100)
+        second_random_int = random.randint(0, 100)
+        result = first_random_int * second_random_int
+        self.assertEqual(self.calculate("*", first_random_int, second_random_int), result)
+
+    def test_random_div(self):
+        first_random_int = random.randint(0, 100)
+        second_random_int = random.randint(0, 100)
+        result = first_random_int / second_random_int
+        self.assertEqual(self.calculate("/", first_random_int, second_random_int), result)
+
+
+    def calculate(self, userAction, numberA, numberB):
+        if userAction == "+":
+            return(numberA + numberB)
+        elif userAction == "-":
+            return(numberA - numberB)
+        elif userAction == "*":
+            return(numberA * numberB)
+        elif userAction == "/":
+            while numberB == 0:
+                return("Input number B. NumberB MUST be > 0")
+                return("Input number B")
                 numberB = int(input())
-            return print(numberA / numberB)
-    print(numberA / numberB)
+            return (numberA / numberB)
+        else:
+            return "default case"
 
-
-calculate(userAction, numberA, numberB)
+if __name__ == '__main__':
+    unittest.main()
