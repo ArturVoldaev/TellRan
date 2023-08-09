21dbca5a73bcdca088340b45cdc105833e622f82
ArturVoldaev <79632566+ArturVoldaev@users.noreply.github.com>
2023-08-09 11:33:01 -0400

    made HW5

---
+++ Algoritms/HomeWork/homework5/HomeWork5.java
@@ -0,0 +1,53 @@
+package homework5;
+
+import java.util.Arrays;
+
+public class HomeWork5 {
+  public static void main(String[] args) {
+    int[] array = {7, -6, 4, 1, 6, 8, -2, 0};
+    int low = 0;
+    int high = array.length - 1;
+    quickSort(array, low, high);
+    System.out.println(Arrays.toString(array));
+  }
+
+  static void quickSort(int[] array, int low, int high) {
+    if (array.length == 0) {
+      return;
+    }
+
+    if (low >= high) {
+      return;
+    }
+
+    // выбрать опорный элемент
+    int middle = low + (high - low) / 2;
+    int opora = array[middle];
+
+    // разделим на подмассивы, слева - меньше опорного, справа - больше
+
+    int i = low, j = high;
+    while (i <= j) {
+      while (array[i] < opora){
+        i++;
+      }
+      while (array[j] > opora){
+        j--;
+      }
+      if(i <= j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+        i++;
+        j--;
+      }
+      // вызов рекурсии для левой и правой части
+      if (low < j) {
+        quickSort(array, low, j);
+      }
+      if (high > i) {
+        quickSort(array, i, high);
+      }
+    }
+  }
+}
