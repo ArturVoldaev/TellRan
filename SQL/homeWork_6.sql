41bccc6325dc56f3b00d1c0caceee8b409169563
ArturVoldaev <79632566+ArturVoldaev@users.noreply.github.com>
2023-10-24 15:02:52 -0400

    add hw6

---
+++ SQL/homeWork_6.sql
@@ -0,0 +1,53 @@
+use shop;
+
+-- 1
+
+SELECT 
+CNAME
+FROM customers
+WHERE CUST_ID IN (
+	SELECT
+	CUST_ID 
+	FROM orders 
+	WHERE AMT > 1000);
+    
+-- 2
+SELECT 
+CNAME
+FROM customers
+WHERE CUST_ID IN (
+	SELECT
+	CUST_ID 
+	FROM orders 
+	WHERE AMT > 1000);
+    
+-- 3
+
+SELECT 
+t1.SNAME,
+t1.COMM - t2.COMM AS difference
+FROM SELLERS t1
+LEFT JOIN SELLERS t2
+ON t1.SELL_ID = t2.BOSS_ID;
+
+-- 4
+
+SELECT 
+*
+FROM customers;
+    SELECT 
+*
+FROM orders;
+    SELECT 
+*
+FROM sellers;
+
+SELECT
+t1.CNAME,
+t2.SNAME,
+t2.CITY
+FROM customers t1
+INNER JOIN sellers t2
+ON t1.CITY = t2.CITY;
+
+
