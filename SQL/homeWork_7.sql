cdff0d458b0c8ab517b1a824b05a642de027891d
ArturVoldaev <79632566+ArturVoldaev@users.noreply.github.com>
2023-10-26 10:50:34 -0400

    add HW 7

---
+++ SQL/homeWork_7.sql
@@ -0,0 +1,58 @@
+use uni;
+
+/*
+
+Make it as part of homework
+
+8.Вывести имя студента и имена  старост, которые есть на курсах, которые он проходит
+"Display the name of the student and the names of the headman representatives 
+who are present in the courses that he is taking."
+*/
+
+SELECT
+t1.title as course_name,
+t2.name as names_of_headman,
+t3.name as names_of_studen
+FROM Courses t1
+INNER JOIN Students t2
+ON t1.headman_id = t2.id
+INNER JOIN Students t3
+ON t3.id = t1.id;
+
+
+use shop;
+
+-- 2 
+
+SELECT 
+t1.CNAME,
+t3.SNAME
+FROM customers t1
+INNER JOIN orders t2
+ON t1.CUST_ID = t2.CUST_ID
+INNER JOIN sellers t3
+ON t2.SELL_ID = t3.SELL_ID;
+
+
+-- 3
+SELECT * 
+FROM customers;
+
+SELECT * 
+FROM orders;
+
+SELECT 
+t1.CNAME
+FROM customers t1
+LEFT JOIN orders t2
+ON t1.CUST_ID = t2.CUST_ID
+WHERE ORDER_ID IS NULL;
+
+SELECT 
+CNAME
+FROM customers
+WHERE CUST_ID NOT IN (
+	SELECT CUST_ID FROM orders
+);
+
+
